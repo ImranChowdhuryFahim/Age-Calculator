804a446e647d2de7613a20b5e8a4528c6705b9ca
ImranChowdhuryFahim <imran.cuet.cse17@gmail.com>
2021-03-23 11:46:43 +0600

    made the dashboard scrollable and disabled default dark settings

--- app/src/main/java/com/brainfluence/agecalculator/MainActivity.java
+++ app/src/main/java/com/brainfluence/agecalculator/MainActivity.java
@@ -2,6 +2,7 @@ package com.brainfluence.agecalculator;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.cardview.widget.CardView;
 
 import android.content.DialogInterface;
@@ -26,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         MobileAds.initialize(this, new OnInitializationCompleteListener() {
             @Override
             public void onInitializationComplete(InitializationStatus initializationStatus) {
